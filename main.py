e5848676d41d6a0936a098d3d4483b8d73e733be
theshepherd123 <22649183+theshepherd123@users.noreply.github.com>
2021-11-13 13:00:26 -0600

    initial

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    df = pd.DataFrame({'X':[22,33,44], 'Y':[123,22,344]})
+    print(df)
+    a = np.array([1,2,3])
+    print(a)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
