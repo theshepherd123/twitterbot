7647f7a8ffff29310490ca7df5a7b974fcf0233c
theshepherd123 <22649183+theshepherd123@users.noreply.github.com>
2021-11-13 13:03:33 -0600

    added lucas

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pandas as pd
 import numpy as np
+
+
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def print_hi(name):
-    df = pd.DataFrame({'X':[22,33,44], 'Y':[123,22,344]})
+    df = pd.DataFrame({'X': [22, 33, 44], 'Y': [123, 22, 344]})
     print(df)
-    a = np.array([1,2,3])
+    a = np.array([1, 2, 3])
     print(a)
+
+
+def print_lucas():
+    print("I AM PRINTING LUCAS NAME")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_lucas()
